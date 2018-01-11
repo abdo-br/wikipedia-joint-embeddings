3e57680a586a24476bd4bab9e0a8053b195167b6
Abdulrahman Bres <bres.a@hotmail.com>
2018-01-11 19:16:36 +0100

    Optimize extraction of entities stats

--- parallel_extract_mentions_stats.py
+++ parallel_extract_mentions_stats.py
@@ -17,59 +17,49 @@ def data_initialize():
 
     global entities
     entities = util.get_entities(chunks=True)
-    entities.set_index(['article', 'entity'], inplace=True)
+    #entities.set_index(['article', 'entity'], inplace=True)
     print('Entities loaded!')
 
 
 def extract(article):
 
-    if len(article.page_name) > 250:
-        return
-
     print(article.page_name.encode('utf8'), '\n')
 
     # get the linked entities within the article
-    #article_entities = entities[entities['article'] == article.page_id, 'entity']
-    #article_entities = entities.loc[entities.article.str.match(article.page_id), 'entity']
-    #article_entities = entities.loc[entities.article == article.page_id, 'entity']
-    try:
-        article_entities = entities.loc[(article.page_id,)].index
-    except:
-        with open(settings.PATH_DATAOBJECTS + 'unfound_articles.txt', 'a', encoding='utf-8') as err:
-            err.write(article.page_name)
-            err.write('\n')
-            err.close()
-            return
+    article_entities = entities.loc[entities.article == article.page_id, 'entity']
 
-    # get the article content
+#    try:
+#        article_entities = entities.loc[(article.page_id,)].index
+#    except:
+#        with open(settings.PATH_DATAOBJECTS + 'unfound_articles.txt', 'a', encoding='utf-8') as err:
+#            err.write(article.page_name)
+#            err.write('\n')
+#            err.close()
+#            return
+
     article_body = article.to_string()
 
     G = pd.DataFrame(columns=['article', 'entity', 'mention'], dtype='unicode')
 
     for entity in article_entities:
 
-        try:
-            pattern = nlp.get_entity_pattern(entity)
+        pattern = nlp.get_entity_pattern(entity)
 
-            for pair in re.finditer(pattern, article_body):
-                mention, entity = pair.group()[1:].split(']')
-                entity = entity[1:-1]
-                if nlp.invalid_entity(entity):
-                    continue
-                if mention.strip() == '' or len(mention) > 249:
-                    continue
+        for pair in re.finditer(pattern, article_body):
+            mention, entity = pair.group()[1:].split(']')
+            entity = entity[1:-1]
+            if nlp.invalid_entity(entity):
+                continue
 
-                G.loc[len(G.index)] = [article.page_name, entity, mention]
+            G.loc[len(G.index)] = [article.page_name, entity, mention]
 
-                # we should strip mentions from ''
+            # we should strip mentions from ''
 
-        except:
-            pass
-
-    G.to_hdf(settings.PATH_DATAOBJECTS + str(os.getpid()) + '.hdf5',
-             format='table', append=True, data_columns=True,
+    G.to_hdf(settings.PATH_DATAOBJECTS + str(os.getpid()) + '.hdf5', format='table',
+             append=True, complib='blosc', complevel=3,
              key='articles_entities_mentions_graph', encoding='utf-8',
-             min_itemsize={'article': 300, 'entity': 300, 'mention': 300})
+             min_itemsize={'article': 500, 'entity': 500, 'mention': 500})
+
 
 if __name__ == '__main__':
 
@@ -82,7 +72,7 @@ if __name__ == '__main__':
     #partial_extract = partial(extract, ns.entities)
     #partial_extract = partial(extract, get_article_entities(entities, None))
 
-    p = Pool(processes=14, initializer=data_initialize, initargs=(), maxtasksperchild=200)
+    p = Pool(processes=3, initializer=data_initialize, initargs=(), maxtasksperchild=200)
 
     with open(settings.PATH_ARTICLES, 'rb') as a:
 
