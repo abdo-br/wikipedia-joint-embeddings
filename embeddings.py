215716c560f8d20afcd71ffd15bc64b422a3ab16
Abdulrahman Bres <bres.a@hotmail.com>
2018-03-19 02:41:50 +0100

    Add more parameters to embeddings

--- embeddings.py
+++ embeddings.py
@@ -14,11 +14,14 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 
 class Sentences():
 
+    def __init__(self, directory):
+        self.directory = directory
+
     def __iter__(self):
 
-        for file in os.scandir(settings.PATH_OUTPUT):
+        for file in os.scandir(self.directory):
             if file.name.endswith('.txt'):
-                with open(file.path, 'r') as a:
+                with open(file.path, 'r', encoding='utf-8') as a:
                     paragraphs = nlp.get_paragraphs(a.read())
                     for p in paragraphs:
                         sentences = nlp.get_sentences(p)
@@ -27,9 +30,9 @@ class Sentences():
                             yield sentence.lower().split()
 
 
-def build(dimensions, num_workers):
-    model = gensim.models.Word2Vec(sentences=Sentences(), size=dimensions,
-                                   window=5, min_count=5, sg=0,
+def build(corpus, dimensions, window, num_workers):
+    model = gensim.models.Word2Vec(sentences=Sentences(corpus), size=dimensions,
+                                   window=window, min_count=5, sg=0,
                                    workers=num_workers)
     return model
 
@@ -38,18 +41,25 @@ if __name__ == '__main__':
 
     __spec__ = None
 
+    corpus = ''
     size = 50
-    cores = 3
+    window = 5
+    cores = 2
 
-    opts, args = getopt.getopt(sys.argv[1:], 's:c:')
+    opts, args = getopt.getopt(sys.argv[1:], 'd:s:c:')
     for opt, arg in opts:
+        if opt == '-d':
+            corpus = arg
         if opt == '-s':
             size = int(arg)
         if opt == '-c':
             cores = int(arg)
 
-    w2v_model = build(size, cores)
-    w2v_model.save(settings.PATH_DATAOBJECTS + 'w2v/model_%s__%s' % (size, time.strftime('%d-%m-%Y')))
+    print('workers: ' + str(cores))
+    print('corpus: ' + corpus)
+
+    w2v_model = build(settings.PATH_OUTPUT+corpus, size, window, cores)
+    w2v_model.save(settings.PATH_OUTPUT + 'w2v/skipgram_%s_%s_%s__%s' % (corpus, size, window, time.strftime('%d-%m-%Y')))
 
     print('Done!')
 
