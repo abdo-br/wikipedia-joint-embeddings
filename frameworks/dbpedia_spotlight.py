5f4687a167a6ac560d217381c060230b2777fd5b
Abdulrahman Bres <bres.a@hotmail.com>
2018-03-19 02:40:34 +0100

    Add "support" parameter to dbpedia spotlight

--- frameworks/dbpedia_spotlight.py
+++ frameworks/dbpedia_spotlight.py
@@ -3,12 +3,13 @@ import os
 import json
 import nlp_util as nlp
 import pandas as pd
+import gold_standard as gs
 
 
-def annotate(text, confidence):
+def annotate(text, confidence, support):
 
-    annotations = pd.DataFrame(columns=['Mention', 'EntityID',
-                                        'Offset', 'Sentence', 'The_Sentence'],
+    annotations = pd.DataFrame(columns=['mention', 'entity', 'entity_id',
+                                        'offset', 'sentence', 'the_sentence'],
                                dtype='unicode', index=None)
 
     sentences_spans = []
@@ -16,17 +17,23 @@ def annotate(text, confidence):
     for sentence in nlp.get_sentences_spans(text, tokenized_sents):
         sentences_spans.append(sentence)
 
-    query = r'curl http://model.dbpedia-spotlight.org/en/annotate --data-urlencode "text={}" --data "confidence={}" -H "Accept: application/json"'.format(text, confidence)
+    query = r'curl http://model.dbpedia-spotlight.org/en/annotate --data-urlencode "text={}" --data "confidence={}&support={}" -H "Accept: application/json"'.format(text, confidence, support)
 
     data = json.loads(os.popen(query).read())
 
-    for entity in data['Resources']:
-        sentence_num, sentence_txt = nlp.get_sentence_number(
-                sentences_spans, int(entity['@offset']))
+    if 'Resources' not in data:
+        return annotations
 
-        annotations.loc[len(annotations.index)] = [entity['@surfaceForm'],
-                                                   entity['@URI'][28:],
-                                                   entity['@offset'],
+    for annotation in data['Resources']:
+        sentence_num, sentence_txt = nlp.get_sentence_number(
+                sentences_spans, int(annotation['@offset']))
+
+        entity = annotation['@URI'][28:].replace('_', ' ')
+
+        annotations.loc[len(annotations.index)] = [annotation['@surfaceForm'],
+                                                   entity,
+                                                   nlp.get_entity_id(entity),
+                                                   annotation['@offset'],
                                                    sentence_num,
                                                    sentence_txt]
     return annotations
@@ -34,30 +41,27 @@ def annotate(text, confidence):
 
 def get_annotations(gold_standard):
 
-    # Check entity ID
-
-    annotations = pd.DataFrame(columns=['Article', 'Mention', 'EntityID',
-                                        'Offset', 'Sentence', 'The_Sentence'],
+    annotations = pd.DataFrame(columns=['article', 'mention', 'entity', 'entity_id',
+                                        'offset', 'sentence', 'the_sentence'],
                                dtype='unicode', index=None)
 
-    for article in gold_standard:
+    for article in gold_standard.articles:
         print(article.title)
         in_article = 0
         article_paragraphs = nlp.get_paragraphs(article.text)
         for paragraph in article_paragraphs:
-            article_annotations = annotate(paragraph, 0.35)
-            article_annotations['Article'] = article.title
-            article_annotations['Sentence'] = article_annotations['Sentence'].map(lambda x: x + in_article)
+            article_annotations = annotate(paragraph, 0.6, 1)
+            article_annotations['article'] = article.title
+            article_annotations['sentence'] = article_annotations['sentence'].map(lambda x: x + in_article)
             annotations = annotations.append(article_annotations)
             in_article += len(article_annotations)
 
-        return annotations
+    return annotations
 
 # test
+#txt = r'''President Obama called Wednesday on Congress to extend the tax break. \n Afterwards he met his wife Michelle'''
 
-# iteate and query per paragrtaph
-# TODO
+#G = annotate(txt, 0.35, 0)
 
-txt = r'''President Obama called Wednesday on Congress to extend the tax break. \n Afterwards he met his wife Michelle'''
-
-G = annotate('title', txt, 0.35)
+#gold_std = gs.gold_standard()
+#annotations = get_annotations(gold_std)
